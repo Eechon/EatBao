74f28620aa6925201d282981b5394cac722e11be
haoxiangzi <1964835642@qq.com>
2016-11-30 12:53:21 +0800

    商店各个页面部分信息更改

---
+++ script/business/business_center.js
@@ -0,0 +1,72 @@
+// 获得列表
+var table = document.getElementById("list");
+// 新建数组列表
+function create_array() {
+    var i = 0;
+    var j = 0;
+    var table_title = document.getElementById("title1");
+    table_title.innerHTML = "订单总汇";
+    var list = [
+        ["名称", "单价(元)", "数量(份)", "总价(元)", "下单时间"]
+    ]
+    for (j = 0; j < 5; j++) {
+        var td0 = table.rows[0].cells[j]; //获得table第一行
+        td0.innerHTML = list[0][j];
+    }
+
+
+
+    var list = new Array();
+    for (i = 1; i < 9; i++) {
+        list[i] = new Array();
+        for (j = 0; j < 5; j++) {
+            var td = table.rows[i].cells[j]; //获得table的行列
+            list[i][j] = i + j;
+            td.innerHTML = list[i][j];
+        }
+
+    }
+}
+setTimeout(function() {
+    document.getElementById('content').style.height = window.innerHeight + 'px';
+}, 20);
+
+function tiaozhuan() {
+    var s = document.getElementById("danjia");
+    s.innerHTML = "200";
+}
+// 店铺订单
+function all_order() {
+    table.innerHTML = table.innerHTML;
+}
+// 未接订单
+function open_order() {
+    create_array();
+
+}
+// 退款定单
+function refund_order() {
+    create_array();
+
+}
+// 完成订单
+function complete_order() {
+    create_array();
+
+}
+// 商家商品
+function goods() {
+    var table_title = document.getElementById("title1");
+    table_title.innerHTML = "商家商品";
+
+    var i = 0;
+    var j = 0;
+
+    var list = [
+        ["名称", "类型", "单价", "库存", "操作"]
+    ]
+    for (j = 0; j < 5; j++) {
+        var td0 = table.rows[0].cells[j]; //获得table第一行
+        td0.innerHTML = list[0][j];
+    }
+}
