3807c280729c2887c08729afc6e7c8470e5fee7d
hxqtohxq <798555920@qq.com>
2016-12-05 16:44:05 +0800

    x

---
+++ script/classification.js
@@ -0,0 +1,13 @@
+
+
+
+
+$(function(){
+	 $.get("http://localhost:9001/classification/all", function(result){
+		
+		result.resultParm.classificationList.forEach((classification)=>{
+			console.log(classification);
+			$("#classification").append('<li role="presentation"><a href="#">'+classification.name+'</a></li>');
+		});
+	});
+});
