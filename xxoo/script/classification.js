e8b4d567f511efedb20d248f2e04e9721e2ebe4b
hxqtohxq <798555920@qq.com>
2016-12-06 21:28:42 +0800

    x

--- xxoo/script/classification.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    // 获得商品分类导航栏
-    $.get(baseUrl + "/classification/all", function(result) {
-        result.resultParm.classificationList.forEach((classification) => {
-            console.log(classification);
-            $("#classification").append('<li role="presentation"><a href="#">' + classification.name + '</a></li>');
-        });
-    });
-});
--- xxoo/script/index/index.js
+++
@@ -1,2 +0,0 @@
-var s = document.getElementsByTagName("body");
-s.style.height = window.innerHeight +'px';
