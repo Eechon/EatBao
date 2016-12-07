8ca38d9c17ff9825117625258fefdad00dc55792
hxqtohxq <798555920@qq.com>
2016-12-07 17:38:31 +0800

    x

--- js/goods.js
+++ js/goods.js
@@ -1,7 +1,7 @@
 ﻿
 $(function(){
 	//获得商家下的商品
-	 $.get(baseUrl + "/goods/shop/1", function(result){
+	 $.get(baseUrl + "/goods/shop/"+ getParam("shopId"), function(result){
 		 var block;
 		 var count = 0;
 		result.resultParm.goodsList.forEach((goods)=>{
@@ -57,4 +57,23 @@ function showText(name,id,price,img){
 	$('#goodsName').text(name);
 	$("#goodsId").val(id);
 	$("#price").val(price);
+}
+
+function getParam(paramName) {
+    paramValue = "";
+    isFound = false;
+    if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
+        arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&");
+        i = 0;
+        while (i < arrSource.length && !isFound) {
+            if (arrSource[i].indexOf("=") > 0) {
+                if (arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase()) {
+                    paramValue = arrSource[i].split("=")[1];
+                    isFound = true;
+                }
+            }
+            i++;
+        }
+    }
+    return paramValue;
 }
--- js/index.js
+++ js/index.js
@@ -1,16 +1,8 @@
 ﻿
-$(function(){
-	//获得商品分类
-	 $.get(baseUrl + "/classification/all", function(result){
-		result.resultParm.classificationList.forEach((classification)=>{
-			console.log(classification);
-			//$("#classification").append('<li role="presentation"><a href="#">'+classification.name+'</a></li>');
-		});
-	});
-	//获得商家
-	 $.get(baseUrl + "/shop/all", function(result){
 
-		 
+$(function(){	
+     //获得商家
+	 $.get(baseUrl + "/shop/all", (result)=>{
 		 var block;
 		 var count = 0;
 		result.resultParm.shopList.forEach((shop)=>{
@@ -21,7 +13,7 @@ $(function(){
 				$($(".container")[1]).append(block);
 				//block = $("<div class='offer-bottom' style='margin-bottom:30px' ></div>");
 			}
-			block.append('<div class="col-md-3 offer-left"><a href="goods.html"><img src="images/o-5.jpg" alt=""/><h6>'+shop.shopName+'</h6></a><h4><a href="goods.html">'+shop.shopName+'</a></h4><div class="o-btn"><a href="goods.html">进入商家</a></div></div>');
+			block.append('<div class="col-md-3 offer-left"><a href="goods.html?shopId='+shop.id+'"><img src="images/o-5.jpg" alt=""/><h6>'+shop.shopName+'</h6></a><h4><a href="goods.html?shopId='+shop.id+'">'+shop.shopName+'</a></h4><div class="o-btn"><a href="goods.html?shopId='+shop.id+'">进入商家</a></div></div>');
 			if(count%4 ==3){
 				block.append('<div class="clearfix"> </div>');
 			}
