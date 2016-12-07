de865dc1a5b58d0c497f3953994ea8426164e1f1
haoxiangzi <1964835642@qq.com>
2016-12-07 09:40:23 +0800

    x

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,7 @@
 //引入插件
 
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var minifycss = require('gulp-minify-css');
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var notify = require('gulp-notify');
-var cache = require('gulp-cache');
-var del = require('del');
 var connect = require('gulp-connect');
-
-// Scripts
-gulp.task('scripts', function() {
-  return gulp.src('src/scripts/**/*.js')
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(notify({ message: 'Scripts task complete' }));
-});
 
 //创建watch任务去检测html文件,其定义了当html改动之后，去调用一个Gulp的Task
 gulp.task('watch', function () {
@@ -44,6 +21,11 @@ gulp.task('connect', function () {
 
 gulp.task('html', function () {
     gulp.src('./*/*.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('scripts', function () {
+    gulp.src('./*/*.js')
         .pipe(connect.reload());
 });
 
