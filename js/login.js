72cf4b089c4eb3319adbdf89ff891392ba844276
hxqtohxq <798555920@qq.com>
2016-12-07 17:38:48 +0800

    x

---
+++ js/login.js
@@ -0,0 +1,13 @@
+﻿$(function(){
+	$("#login").on("click",()=>{
+		//13570956669
+		$.post(baseUrl+"/users/doLogin",{"account":$("#phone").val(),"password":$("#password").val()},(result)=>{
+			console.log(result);
+			if(null != result.resultParm.user){
+				window.location.href="/index.html";
+			}else{
+				alert("登录失败");
+			}
+		});
+	});
+});
