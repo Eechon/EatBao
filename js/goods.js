d9bf5a3ca9019bda15866bc65ff3e620cdb427b4
hxqtohxq <798555920@qq.com>
2016-12-09 17:18:59 +0800

    x

--- js/goods.js
+++ js/goods.js
@@ -60,7 +60,7 @@ function showText(name,id,price,img){
 	$("#price").val(price);
 }
 
-function paramName) {
+function getParam(paramName) {
     paramValue = "";
     isFound = false;
     if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
