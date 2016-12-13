f485273c07ef135b3022f0963e063a3fedfea30f
hxqtohxq <798555920@qq.com>
2016-12-13 15:36:10 +0800

    x

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ $(function(){
 				$($(".container")[1]).append(block);
 				//block = $("<div class='offer-bottom' style='margin-bottom:30px' ></div>");
 			}
-			block.append('<div class="col-md-3 offer-left"><a href="goods.html?shopId='+shop.id+'"><img src="images/o-5.jpg" alt=""/><h6>'+shop.shopName+'</h6></a><h4><a href="goods.html?shopId='+shop.id+'">'+shop.shopName+'</a></h4><div class="o-btn"><a href="goods.html?shopId='+shop.id+'">进入商家</a></div></div>');
+			block.append('<div class="col-md-3 offer-left"><a href="goods.html?shopId='+shop.id+'"><img src="'+baseUrl + shop.shopImg +'" alt=""/><h6>'+shop.shopName+'</h6></a><h4><a href="goods.html?shopId='+shop.id+'">'+shop.shopName+'</a></h4><div class="o-btn"><a href="goods.html?shopId='+shop.id+'">进入商家</a></div></div>');
 			if(count%4 ==3){
 				block.append('<div class="clearfix"> </div>');
 			}
